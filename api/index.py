1844348b820566b1b032bdb3ba163bddc91a11be
Bethlydia Mutuba <mutubaneema@gmail.com>
2024-05-04 12:33:04 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+import qrcode
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        print(url)
+        if url:
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(url)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+            img_io = io.BytesIO()
+            img.save(img_io, 'PNG')
+            img_io.seek(0)
+            return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='qr_code.png')
+    return render_template('setlink.html')
